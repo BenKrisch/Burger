839308bf20b1f7b0cd7f09dbd8ecab62e9491ecf
Benjamin Krisch <bkrisch@yourmembership.com>
2017-04-29 19:29:02 -0500

    First Commit

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('I Fought the Slaw Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Kales from the Crypt Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Goodnight and Good Leek Burger', false); 
+
+SELECT * FROM burgers;
